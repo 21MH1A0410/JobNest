6c00f03e3d04422e311da82599b0b7b01d975108
KODANDA RAMA RAJU BUDDHARAJU <98527269+21MH1A0410@users.noreply.github.com>
2025-02-05 22:48:39 +0530

    Update protected-route.jsx

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -5,16 +5,7 @@ const ProtectedRoute = ({children}) => {
   const {isSignedIn,user,isLoaded} =useUser();
   const {pathname} = useLocation();
 
-  if(isLoaded && !isSignedIn && isSignedIn!==undefined){
-    return <Navigate to='/?sign-in=true'/>;
-  }
 
-  if(user!==undefined && !user.unsafeMetadata.role && pathname!=='/onboarding')
-  {
-    return <Navigate to='/onboarding'/>;
-  }
-  return children;
-};
 
 
 export default ProtectedRoute
